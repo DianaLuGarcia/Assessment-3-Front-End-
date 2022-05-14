cdb3e30243af39cd9a34d5f889addb974bc72d94
Diana Lupita Garcia <75147104+DianaLuGarcia@users.noreply.github.com>
2022-05-13 22:55:48 -0600

    mouseover working

--- about.js
+++ about.js
@@ -1,11 +1,26 @@
 console.log("hello world");
 
-const submitBtn = document.querySelector(#'')
+//Add an event listener that listens for a mouseover event.
+// When the user mouses over the picture on the page (cat or rubber duck)
+//The page should alert give the user a compliment.
+
+//const img = document.querySelector("cat");
+//const img = document.getElementById("cat");
+document.getElementById("cat").addEventListener("mouseover", compliment);
+
+function compliment() {
+  alert("Hello Beautiful!");
+}
+
+//Change the event listener that is listening for the Contact Us form to submit.
+//Instead of doing a console.log message on submit, the form should alert the user that the form has been submitted successfully.
+//Use the alert function in Javascript.
 function handleSubmit(evt) {
   evt.preventDefault();
-
-  console.log("form submit");
-};
+  //console.log("form submit");
+  console.log(1);
+  alert("Form Submitted Successfully!");
+}
 
 let form = document.querySelector("form#contact");
 
